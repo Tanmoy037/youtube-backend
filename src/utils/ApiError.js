8ac27bda61e84e311ff78d2604fc27a5e5730d9b
Tanmoy037 <tanmoysantra67@gmail.com>
2024-04-01 23:52:59 +0530

    utils added for code reuseability

---
+++ src/utils/ApiError.js
@@ -0,0 +1,25 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        messge = "Something went wrong",
+        error = [],
+        statck = ""
+    ){
+        super(messge)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false
+        this.errors = errors
+
+        if (statck) {
+            this.stack = statck
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+
+
+    }
+}
+
+export {ApiError}
